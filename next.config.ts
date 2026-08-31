c88b4c9f28b95471e8f67506ef11f27462db16d3
Andrew Local <andrew.buist99@outlook.com>
2026-08-31 16:50:35 +0100

    config base path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/salty-art",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
