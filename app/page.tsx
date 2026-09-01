9fe0233e8ab2c29dcb585198c6aa31d029324e28
Andrew Local <andrew.buist99@outlook.com>
2026-09-01 13:50:00 +0100

    splash page draft

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center">
-      Website coming soon!
+    <div className="flex flex-col h-screen items-center justify-center space-y-20">
+      <div className="flex items-end">
+        <p className="text-6xl">
+          Website coming soon!
+        </p>
+      </div>
+      <div className="flex flex-col items-center space-y-5">
+        <div className="text-xl">
+          <p>Colour palette</p>
+        </div>
+        <div className="flex justify-evenly space-x-10">
+          <div className="h-10 w-10 bg-blood-1 border-2 border-black"></div>
+          <div className="h-10 w-10 bg-blood-2 border-2 border-black"></div>
+          <div className="h-10 w-10 bg-lilac-1 border-2 border-black"></div>
+          <div className="h-10 w-10 bg-lilac-2 border-2 border-black"></div>
+          <div className="h-10 w-10 bg-purple-1 border-2 border-black"></div>
+        </div>
+      </div>
     </div>
   );
 }
